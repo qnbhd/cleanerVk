4fc8e007acc4b630398f240d1a6a9d2b0180915d
qnbhd <1qnbhd@gmail.com>
2019-05-03 16:19:56 +0300

    fixes

--- library.py
+++ library.py
@@ -1,6 +1,6 @@
 import time
 from messages import messages
-
+import sys
 
 class cleanerVk:
     def __init__(
@@ -31,8 +31,11 @@ class cleanerVk:
                 yield node['id']
 
     def getCandidates(self):
+        animation = "|/-\\"
         for node in self.getConversations():
-            for msg in self.getHistory(node):
+            for i, msg in enumerate(self.getHistory(node)):
+                sys.stdout.write("\r" + animation[i % len(animation)])
+                sys.stdout.flush()
                 yield msg
                 time.sleep(self.sleep_coef)
 
@@ -50,5 +53,7 @@ class cleanerVk:
             if counter % self.send_num == 0 and counter != 0:
                 self.delete_messages(to_del)
                 to_del = []
-        if to_del != '':
+        if to_del != []:
             self.delete_messages(to_del)
+        sys.stdout.flush()
+        print('')
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from library import cleanerVk
-
+import sys
+import time
+from messages import invalid_password
 
 def main():
-    login, password = 'login', 'password'
-    session = cleanerVk(login, password)
-    session.deleteTraces()
 
+    print('Введите логин')
+    login = input()
+    print('Ввведите пароль')
+    password = input()
+    
+    try:
+        session = cleanerVk(login, password)
+        print('Сколько диалогов просмотреть?')
+        session.conversations_count = int(input())
+        print('Сколько в среднем сообщений в день вы отправляете?')
+        session.messages_count = int(input())
+        session.deleteTraces()
+    except invalid_password:
+        print('Ошибка входа')
+
+
+    
 
 if __name__ == '__main__':
     main()
